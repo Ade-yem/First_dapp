20630e61587b4edcfb5cf8d5bca101f21e23968f
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2024-05-13 13:57:49 +0100

    added profile link

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -3,17 +3,37 @@ import { Web3Provider } from "@ethersproject/providers";
 import Link from "next/link"
 import { useRouter } from "next/router";
 import useLocalStorage from "../hooks/useLocalStorage";
+import useContract from "../dapp/contract";
+import { Contract } from "ethers";
+import { useState } from "react";
 
 
 export function Account() {
   const { account, active } = useWeb3React();
   const context = useWeb3React<Web3Provider>();
+  const contract = useContract() as Contract
   const router = useRouter();
   const [deactivated, setDeactivated] = useLocalStorage<true | false>("theme", false);
   const Disconnect = async () => {
     await context.deactivate();
     setDeactivated(true)
     router.push("/login");
+  }
+  const verify = async () => {
+    let profile: "hospital" | "doctor" | "patient";
+    if (!contract) return "Unabble to verify"
+    if (await contract.verifyHospital(context.account) === true) {
+      console.log("Hospital")
+      profile = "hospital"
+    }
+    else if (await contract.verifyDoctor(context.account) === true) {
+      console.log("Doctor")
+      profile = "doctor"}
+    else {
+      console.log("Patient")
+      profile = "patient"
+    }
+    router.push(`${profile}`)  
   }
 
   return (
@@ -37,7 +57,7 @@ export function Account() {
       <div tabIndex={0} className="dropdown-content cursor-pointer z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
       {active ? (
         <ul>
-      <li className=""><Link href={"/profile"}>Profile</Link></li>
+      <li className="" onClick={verify}>Profile</li>
       <li className="" onClick={Disconnect}><Link href={"/"}>Logout</Link></li>
       </ul>) : 
       <Link href={"/login"}><span className="p-2">Login with wallet</span></Link>
